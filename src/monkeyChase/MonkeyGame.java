c41bd81066d6285efce08a82f0e9754e6f1219e8
Brian Koga <brian.koga@Brians-MacBook-Pro.local>
2021-10-07 23:16:51 -0700

    initial commit

---
+++ src/monkeyChase/MonkeyGame.java
@@ -0,0 +1,133 @@
+package monkeyChase;
+
+import java.util.ArrayList;
+
+import jig.Entity;
+import jig.ResourceManager;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * A Simple Game of Breakout.
+ * 
+ * The game has four states: StartUp, Playing, and GameOver, LevelOver the game
+ * progresses through these states based on the user's input and the events that
+ * occur. Each state is different in terms of what is displayed and
+ * what input is accepted.
+ * 
+ * In the playing state, the game displays a moving rectangular "ball" that
+ * bounces off the sides of the game container and the blocks that are spawned
+ * based on which level. The user controls a paddle at the bottom of the screen.
+ * If the ball falls off the bottom of the screen, a life is lost and the ball
+ * respawns on top of the paddle. If there are no lives left, the game over state
+ * is entered.
+ * 
+ * When the ball bounces, it appears broken for a short time afterwards and an
+ * explosion animation is played at the impact site to add a bit of eye-candy
+ * additionally, we play a short explosion sound effect when the game is
+ * actively being played.
+ * 
+ * The game also tracks the number of blocks that have been destroyed and syncs
+ * the game update loop with the monitor's refresh rate.
+ * 
+ * Graphics resources courtesy of qubodup:
+ * http://opengameart.org/content/bomb-explosion-animation
+ * 
+ * Sound resources courtesy of DJ Chronos:
+ * http://www.freesound.org/people/DJ%20Chronos/sounds/123236/
+ * 
+ * 
+ * @authors Brian Koga
+ * 
+ */
+public class MonkeyGame extends StateBasedGame {
+	
+	public static final int STARTUPSTATE = 0;
+	public static final int PLAYINGSTATE = 1;
+	public static final int GAMEOVERSTATE = 2;
+	public static final int LEVELOVERSTATE = 3;
+
+	public static final String GAMEOVER_BANNER_RSC = "monkeyChase/resource/gameover.png";
+	public static final String STARTUP_BANNER_RSC = "monkeyChase/resource/PressSpace.png";
+
+	public static final String Tree_RSC = "monkeyChase/resource/minitree.png";
+
+
+	public final int ScreenWidth;
+	public final int ScreenHeight;
+
+
+
+	int lives;
+	int level;
+	int score;
+
+	int tileSize;
+
+	int maxLevel = 3;
+
+
+	/**
+	 * Create the BounceGame frame, saving the width and height for later use.
+	 * 
+	 * @param title
+	 *            the window's title
+	 * @param width
+	 *            the window's width
+	 * @param height
+	 *            the window's height
+	 */
+	public MonkeyGame(String title, int width, int height) {
+		super(title);
+		ScreenHeight = height;
+		ScreenWidth = width;
+
+		Entity.setCoarseGrainedCollisionBoundary(Entity.AABB);
+
+		lives = 3;
+		level = 0;
+		score = 0;
+		tileSize = 32;
+	}
+
+
+	@Override
+	public void initStatesList(GameContainer container) throws SlickException {
+		addState(new StartUpState());
+		addState(new GameOverState());
+		addState(new PlayingState());
+		addState(new LevelOverState());
+		
+		// the sound resource takes a particularly long time to load,
+		// we preload it here to (1) reduce latency when we first play it
+		// and (2) because loading it will load the audio libraries and
+		// unless that is done now, we can't *disable* sound as we
+		// attempt to do in the startUp() method.
+
+		// preload all the resources to avoid warnings & minimize latency...
+		ResourceManager.loadImage(GAMEOVER_BANNER_RSC);
+		ResourceManager.loadImage(STARTUP_BANNER_RSC);
+
+		ResourceManager.loadImage(Tree_RSC);
+
+
+	}
+	
+	public static void main(String[] args) {
+		AppGameContainer app;
+		try {
+			app = new AppGameContainer(new MonkeyGame("Monkey See, Monkey Run!", 896, 768));
+			app.setDisplayMode(896, 768, false);
+			app.setVSync(true);
+			app.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	
+}
---
+++ src/monkeyChase/StartUpState.java
@@ -0,0 +1,188 @@
+package monkeyChase;
+
+import java.util.Iterator;
+
+import jig.ResourceManager;
+
+import jig.Vector;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * This state is active prior to the Game starting. In this state, sound is
+ * turned off. The user can only interact with the game by pressing the
+ * w, s or SPACE keys which allows the selection of a level, then transitions
+ * to the Playing State.
+ *
+ * 
+ * Transitions From (Initialization), GameOverState
+ * 
+ * Transitions To PlayingState
+ */
+
+class StartUpState extends BasicGameState {
+	int menuItem = 0;
+	int levelMenuItem = 1;
+
+	@Override
+	public void init(GameContainer container, StateBasedGame game)
+			throws SlickException {
+	}
+	
+	@Override
+	public void enter(GameContainer container, StateBasedGame game) {
+		container.setSoundOn(false);
+
+		MonkeyGame mg = (MonkeyGame)game;
+
+		mg.lives = 3;
+		mg.score = 0;
+	}
+
+
+	@Override
+	public void render(GameContainer container, StateBasedGame game,
+			Graphics g) throws SlickException {
+		MonkeyGame mg = (MonkeyGame)game;
+
+
+		g.drawString("Brian Koga", 10, mg.ScreenHeight -20);
+
+
+
+		// simulate choosing an option, this is based on the menuItem variable
+		// it can be 0, 1, 2 or 3. if it is 0, the start is focused, if it is 1, the level is focused
+		// if it is 2, demo is focused and if it is 2, then the person is choosing levels
+
+		if(menuItem == 0) {
+			g.drawString("< Start >", mg.ScreenWidth/2 -30, mg.ScreenHeight/2 -20);
+			g.drawString("  Levels ", mg.ScreenWidth/2 - 30, (mg.ScreenHeight/2 + 20));
+			g.drawString("   Demo  ", mg.ScreenWidth/2 - 30, (mg.ScreenHeight/2 + 60));
+		// levels focused
+		} else if(menuItem == 1) {
+			g.drawString("  Start  ", mg.ScreenWidth / 2 - 30, mg.ScreenHeight / 2 - 20);
+			g.drawString("< Levels >", mg.ScreenWidth/2 - 30, (mg.ScreenHeight/2 + 20));
+			g.drawString("   Demo  ", mg.ScreenWidth/2 - 30, (mg.ScreenHeight/2 + 60));
+		// in the level select menu
+		} else if (menuItem == 2) {
+			g.drawString("  Start  ", mg.ScreenWidth / 2 - 30, mg.ScreenHeight / 2 - 20);
+			g.drawString("  Levels  ", mg.ScreenWidth/2 - 30, (mg.ScreenHeight/2 + 20));
+			g.drawString("<  Demo >", mg.ScreenWidth/2 - 30, (mg.ScreenHeight/2 + 60));
+		} else if (menuItem == 3) {
+			// level 1 focused
+			if (levelMenuItem == 1) {
+				g.drawString("< Level 1 >", mg.ScreenWidth / 2 - 30, mg.ScreenHeight / 2 - 20);
+				g.drawString("  Level 2  ", mg.ScreenWidth / 2 - 30, (mg.ScreenHeight / 2));
+				g.drawString("  Level 3  ", mg.ScreenWidth / 2 - 30, (mg.ScreenHeight / 2 + 20));
+				g.drawString("   Back   ", mg.ScreenWidth / 2 - 30, (mg.ScreenHeight / 2 + 40));
+				// level 2 focused
+			} else if (levelMenuItem == 2) {
+				g.drawString("  Level 1  ", mg.ScreenWidth / 2 - 30, mg.ScreenHeight / 2 - 20);
+				g.drawString("< Level 2 >", mg.ScreenWidth / 2 - 30, (mg.ScreenHeight / 2));
+				g.drawString("  Level 3  ", mg.ScreenWidth / 2 - 30, (mg.ScreenHeight / 2 + 20));
+				g.drawString("   Back   ", mg.ScreenWidth / 2 - 30, (mg.ScreenHeight / 2 + 40));
+				// level 3 focused
+			} else if (levelMenuItem == 3) {
+				g.drawString("  Level 1  ", mg.ScreenWidth / 2 - 30, mg.ScreenHeight / 2 - 20);
+				g.drawString("  Level 2  ", mg.ScreenWidth / 2 - 30, (mg.ScreenHeight / 2));
+				g.drawString("< Level 3 >", mg.ScreenWidth / 2 - 30, (mg.ScreenHeight / 2 + 20));
+				g.drawString("   Back   ", mg.ScreenWidth / 2 - 30, (mg.ScreenHeight / 2 + 40));
+			} else if (levelMenuItem == 0) {
+				g.drawString("  Level 1  ", mg.ScreenWidth / 2 - 30, mg.ScreenHeight / 2 - 20);
+				g.drawString("  Level 2  ", mg.ScreenWidth / 2 - 30, (mg.ScreenHeight / 2));
+				g.drawString("  Level 3  ", mg.ScreenWidth / 2 - 30, (mg.ScreenHeight / 2 + 20));
+				g.drawString(" < Back > ", mg.ScreenWidth / 2 - 30, (mg.ScreenHeight / 2 + 40));
+			}
+		}
+	}
+
+	@Override
+	public void update(GameContainer container, StateBasedGame game,
+			int delta) throws SlickException {
+
+		Input input = container.getInput();
+		MonkeyGame mg = (MonkeyGame)game;
+
+		// cheats
+		if (input.isKeyPressed(Input.KEY_1)) {
+			mg.level = 1;
+			game.enterState(MonkeyGame.PLAYINGSTATE);
+			// start level 2
+		} else if (input.isKeyPressed(Input.KEY_2)) {
+			mg.level = 2;
+			game.enterState(MonkeyGame.PLAYINGSTATE);
+			// start level 3
+		} else if (input.isKeyPressed(Input.KEY_3)) {
+			mg.level = 3;
+			game.enterState(MonkeyGame.PLAYINGSTATE);
+		}
+
+		// in the level select menu
+		// here, the options are which level to start on and a back
+		if(menuItem == 3) {
+			// move the selector down
+			if (input.isKeyPressed(Input.KEY_S)) {
+				levelMenuItem++;
+				if (levelMenuItem == 4) {
+					levelMenuItem = 0;
+				}
+			// move the selector up
+			} else if (input.isKeyPressed(Input.KEY_W)) {
+				levelMenuItem--;
+				if (levelMenuItem == -1) {
+					levelMenuItem = 3;
+				}
+			// choose an option
+			} else if (input.isKeyPressed(Input.KEY_SPACE)) {
+				// if on back
+				if(levelMenuItem == 0) {
+					// change back to choosing between start and levels
+					menuItem = 0;
+					// set levelMenuItem to 1 so that level one is selected next time
+					levelMenuItem = 1;
+				} else {
+					// if not on back then want to start the level they were highlighting
+					mg.level = levelMenuItem;
+					mg.enterState(MonkeyGame.PLAYINGSTATE);
+				}
+			}
+		} else {
+			// menu item is 0 or 1 or 2, either way, the w and s behavior are the same
+			if (input.isKeyPressed(Input.KEY_S)) {
+				menuItem++;
+				if (menuItem == 3) {
+					menuItem = 0;
+				}
+			} else if (input.isKeyPressed(Input.KEY_W)) {
+				menuItem--;
+				if (menuItem == -1) {
+					menuItem = 2;
+				}
+			} else if (input.isKeyPressed(Input.KEY_SPACE)) {
+				// if space is pressed, different things occur
+				if(menuItem == 0) {
+					// start level 1
+					mg.level = 1;
+					mg.enterState(MonkeyGame.PLAYINGSTATE);
+				} else if(menuItem == 1){
+					// enter level select menu
+					menuItem = 3;
+				} else {
+					// start demo
+					mg.level = 1;
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public int getID() {
+		return MonkeyGame.STARTUPSTATE;
+	}
+	
+}
